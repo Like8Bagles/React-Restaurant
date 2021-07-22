71d4676f6ef15b2c4cfc647a581746fdae2672d0
Anthony <anthonykmbanks@gmail.com>
2021-07-22 05:06:14 -0400

    add orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,42 @@
+class OrdersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    def index
+        user = find_user
+        orders = user.orders
+        render json: orders
+    end
+
+    def show
+        user = find_user
+        order = user.orders.find_by(id: params[:id])
+        render json: order
+    end
+
+    def create
+        user = find_user
+        order = user.orders.create(order_params)
+        if order.valid?
+            render json: order, status: :created
+        else
+            render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = find_user
+        order = user.orders.find_by(id: params[:id])
+        order.delete
+        head :no_content
+    end
+
+    private
+    
+    def order_params
+        params.permit(:order_name)
+    end
+
+    def find_user
+        User.find_by(id: session[:user_id])
+    end
+end
