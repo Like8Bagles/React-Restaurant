aa3fe61b75626e7b99cfcf65da419a6312de7e42
Anthony <anthonykmbanks@gmail.com>
2021-07-24 21:39:44 -0400

    edit Home component

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -4,7 +4,6 @@ const Home = (props) => {
     return (
         <div>
             <h1>This is the Home Page: </h1>
-            <h1>Login in then click "Shows" to add your favorite shows to a list.</h1>
         </div>
     )
 }
